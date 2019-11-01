7eabb4fb6a2596d24e4456eb8b440a2fef75a822
rogovrt <49844050+rogovrt@users.noreply.github.com>
2019-11-02 01:43:49 +0300

    Add files via upload

--- FileUtils.java
+++ FileUtils.java
@@ -3,13 +3,13 @@ import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileUtils implements AutoCloseable{
+public class FileUtils implements AutoCloseable {
     public void close() {
 
     }
 
     static List<String> readAll(String path) {
-        try (BufferedReader buf = new BufferedReader(new FileReader(path))){
+        try (BufferedReader buf = new BufferedReader(new FileReader(path))) {
             List<String> lines = new ArrayList<String>();
             String s = buf.readLine();
             while (s != null) {
@@ -17,10 +17,8 @@ public class FileUtils implements AutoCloseable{
                 s = buf.readLine();
             }
             return lines;
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-            return null;
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
 
@@ -29,36 +27,24 @@ public class FileUtils implements AutoCloseable{
             for (int i = 0; i < lines.size(); i++) {
                 pw.println(lines.get(i));
             }
-        }
-        catch(FileNotFoundException e) {
-            System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
         }
     }
 
     static void copy(String sourceFile, String destinationFile) {
         File source = new File(sourceFile);
         File destination = new File(destinationFile);
-        try (FileChannel sourseChannel = new FileInputStream(source).getChannel()) {
-            try (FileChannel destinationChannel = new FileOutputStream(destination).getChannel()) {
-                destinationChannel.transferFrom(sourseChannel, 0, sourseChannel.size());
-            }
-        }
-        catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (IOException e) {
-            e.getMessage();
+        try (FileChannel sourseChannel = new FileInputStream(source).getChannel();
+             FileChannel destinationChannel = new FileOutputStream(destination).getChannel()) {
+            destinationChannel.transferFrom(sourseChannel, 0, sourseChannel.size());
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
     }
 
     static void delete(String path) {
         File f = new File(path);
-        try {
-            if (!f.delete())
-                throw new FileNotFoundException(path + " (Нет такого файла или каталога)");
-        }
-        catch(FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        f.delete();
     }
 }
